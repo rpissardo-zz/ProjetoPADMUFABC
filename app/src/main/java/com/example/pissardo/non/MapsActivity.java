f41af46ab6cd6103651705d5ea669c5ae9168396
rpissardo <rpissardo@hotmail.com>
2016-11-26 20:33:55 -0200

    Historical Record Feature implemented. Click on botton and change to real time ou real time with record. That's used to detect bus trace.

--- app/src/main/java/com/example/pissardo/non/MapsActivity.java
+++ app/src/main/java/com/example/pissardo/non/MapsActivity.java
@@ -31,6 +31,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private static final String TAG = "Http Connection";
     ToggleButton toggle;
+    int flag = 1;
+    int flagant = 1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -48,24 +50,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setTrafficEnabled(true);
-        toggleMap();
-    }
-
-    public void toggleMap(){
-        toggle = (ToggleButton)findViewById(R.id.toggleButton);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    realTimeMap();
-                } else {
-                    recordMap();
-                }
-            }
-        });
+        updateMap();
 
     }
 
-    public void realTimeMap(){
+
+    public void updateMap(){
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -74,26 +64,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         },0,3000);
     }
 
-    public void recordMap(){
-        new Timer().scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                new MarkerTask2().execute();
-            }
-        },0,3000);
-    }
-
     public void updateCamera(LatLng local){
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(local,18));
     }
 
-    public void updateMarker(LatLng local){
-        mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(local).icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_icon_32)));
-    }
-
-    public void updateMarker2(LatLng local){
-        mMap.addMarker(new MarkerOptions().position(local).icon(BitmapDescriptorFactory.fromResource(R.drawable.dot)));
+    public void updateMarker(final LatLng local){
+        toggle = (ToggleButton)findViewById(R.id.toggleButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    flag = 0;
+                } else {
+                    flag = 1;
+                }
+            }
+        });
+        if(flag==0){
+            flagant = 0;
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(local).icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_icon_32)));
+        }else{
+            if (flagant == 0){
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(local).icon(BitmapDescriptorFactory.fromResource(R.drawable.dot)));
+                flagant = 1;
+            }else{
+                mMap.addMarker(new MarkerOptions().position(local).icon(BitmapDescriptorFactory.fromResource(R.drawable.dot)));
+            }
+        }
     }
 
     public void updateVelocity(int velocity){
@@ -129,83 +127,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     conn.disconnect();
                 }
             }
-            Log.i(TAG, "**********************************************************");
-            Log.i(TAG, json.toString());
             return json.toString();
         }
 
         @Override
         protected void onPostExecute(String json) {
-            Log.i(TAG, "*********************************************************");
-            Log.i(TAG, String.valueOf(json.length()));
             try {
-                // De-serialize the JSON string into an array of city objects
                 JSONObject jsonObj = new JSONObject(json);
-                Log.i(TAG, "*********************************************************");
-                Log.i(TAG, jsonObj.toString());
                 LatLng latLng = new LatLng(jsonObj.getDouble("lat"), jsonObj.getDouble("lon"));
                 int velocity = jsonObj.getInt("vel");
                 String modified = jsonObj.getString("modified");
-                Log.i(TAG, "*********************************************************");
-                Log.i(TAG, latLng.toString());
-                Log.i(TAG, Integer.toString(velocity));
                 updateCamera(latLng);
                 updateMarker(latLng);
-                updateVelocity(velocity);
-                updateModifiedAt(modified);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "Error processing JSON", e);
-            }
-
-        }
-    }
-
-
-    private class MarkerTask2 extends AsyncTask<Void, Void, String> {
-        private static final String LOG_TAG = "ExampleApp";
-        private static final String SERVICE_URL = "https://fretbus-rpissardo.c9users.io/fretbus/data/map2/1";
-        @Override
-        protected String doInBackground(Void... args) {
-            HttpURLConnection conn = null;
-            final StringBuilder json = new StringBuilder();
-            try {
-                URL url = new URL(SERVICE_URL);
-                conn = (HttpURLConnection) url.openConnection();
-                InputStreamReader in = new InputStreamReader(conn.getInputStream());
-                int read;
-                char[] buff = new char[1024];
-                while ((read = in.read(buff)) != -1) {
-                    json.append(buff, 0, read);
-                }
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error connecting to service", e);
-            } finally {
-                if (conn != null) {
-                    conn.disconnect();
-                }
-            }
-            Log.i(TAG, "**********************************************************");
-            Log.i(TAG, json.toString());
-            return json.toString();
-        }
-
-        @Override
-        protected void onPostExecute(String json) {
-            Log.i(TAG, "*********************************************************");
-            Log.i(TAG, String.valueOf(json.length()));
-            try {
-                // De-serialize the JSON string into an array of city objects
-                JSONObject jsonObj = new JSONObject(json);
-                Log.i(TAG, "*********************************************************");
-                Log.i(TAG, jsonObj.toString());
-                LatLng latLng = new LatLng(jsonObj.getDouble("lat"), jsonObj.getDouble("lon"));
-                int velocity = jsonObj.getInt("vel");
-                String modified = jsonObj.getString("modified");
-                Log.i(TAG, "*********************************************************");
-                Log.i(TAG, latLng.toString());
-                Log.i(TAG, Integer.toString(velocity));
-                updateCamera(latLng);
-                updateMarker2(latLng);
                 updateVelocity(velocity);
                 updateModifiedAt(modified);
             } catch (JSONException e) {
